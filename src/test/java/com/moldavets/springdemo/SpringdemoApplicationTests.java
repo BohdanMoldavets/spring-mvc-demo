c5bd63439db9b6c5dc99e614ff80d7edb8b75652
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2024-11-13 19:04:06 +0100

    Initial commit

---
+++ src/test/java/com/moldavets/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldavets.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
